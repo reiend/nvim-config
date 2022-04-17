3e8242fc44be0d396410f42f7d870de825482ae6
jake <loncerasjakerei@gmail.com>
2022-04-17 16:28:20 +0800

    Added lsp config for js and ts

---
+++ lua/user/lsp/settings/tsserver.lua
@@ -0,0 +1,21 @@
+local util = require "lspconfig".util
+
+return {
+  cmd = { "typescript-language-server.cmd", "--stdio" },
+  filetypes = { "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx"
+  },
+  init_options = {
+    hostInfo = "neovim",
+  },
+  root_dir = util.root_pattern(
+    "package.json",
+    "tsconfig.json",
+    "jsconfig.json",
+    ".git"
+  )
+}
